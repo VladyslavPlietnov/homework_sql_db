23b59e08377da92f0d625c6af5b71075a260b78f
Plietnov Vladyslav <pletniov@ukr.net>
2023-11-09 15:45:05 +0100

    second commit

--- find_longest_project.sql
+++ find_longest_project.sql
@@ -1,31 +1,10 @@
-SELECT DATEDIFF(FINISH_DATE, START_DATE), ID
+SELECT ID, DATEDIFF(FINISH_DATE, START_DATE)/30 AS MONTHS
 FROM project
-GROUP BY ID;
-SELECT*FROM project;
-
-CREATE TABLE length_of_project(
-    ID INT PRIMARY KEY, 
-    DAYS INT, 
-    FOREIGN KEY(ID) REFERENCES project(ID)
+WHERE DATEDIFF(FINISH_DATE, START_DATE) = (
+    SELECT MAX(DIFF)
+    FROM(
+        SELECT DATEDIFF(FINISH_DATE, START_DATE) AS DIFF, ID
+        FROM project
+        GROUP BY ID
+    ) AS T
 );
-INSERT length_of_project VALUES(1, NULL);
-INSERT length_of_project VALUES(2, NULL);
-INSERT length_of_project VALUES(3, NULL);
-INSERT length_of_project VALUES(4, NULL);
-INSERT length_of_project VALUES(5, NULL);
-INSERT length_of_project VALUES(6, NULL);
-INSERT length_of_project VALUES(7, NULL);
-INSERT length_of_project VALUES(8, NULL);
-INSERT length_of_project VALUES(9, NULL);
-INSERT length_of_project VALUES(10, NULL);
-
-UPDATE length_of_project 
-SET DAYS = (
-    SELECT DATEDIFF(FINISH_DATE, START_DATE)
-FROM project
-WHERE length_of_project.ID = project.ID
-);
-SELECT*
-FROM length_of_project
-WHERE DAYS = (SELECT MAX(DAYS)
-FROM length_of_project);
--- find_max_projects_client.sql
+++ find_max_projects_client.sql
@@ -1,33 +1,14 @@
-SELECT COUNT(CLIENT_ID), CLIENT_ID
-FROM project
-GROUP BY CLIENT_ID;
-
-CREATE TABLE client_number(
-    CLIENT_ID INT PRIMARY KEY, 
-    COUNT_NUMBER INT, 
-    FOREIGN KEY(CLIENT_ID) REFERENCES project(CLIENT_ID)
-);
-INSERT INTO client_number VALUES(1, NULL);
-INSERT INTO client_number VALUES(2, NULL);
-INSERT INTO client_number VALUES(3, NULL);
-INSERT INTO client_number VALUES(4, NULL);
-INSERT INTO client_number VALUES(5, NULL);
-
-UPDATE client_number
-SET COUNT_NUMBER = (
+SELECT *
+FROM client
+WHERE (
     SELECT COUNT(CLIENT_ID)
     FROM project
-    WHERE project.CLIENT_ID = client_number.CLIENT_ID 
- );
- SELECT*FROM client_number;
-
-SELECT*
-FROM client
-WHERE id = (
-    SELECT CLIENT_ID
-    FROM client_number
-    WHERE COUNT_NUMBER = (
-        SELECT MAX(COUNT_NUMBER)
-        FROM client_number
-    ) AND id = CLIENT_ID
-);
+    WHERE client.id = project.CLIENT_ID
+    ) = (
+       SELECT MAX(COUNTING)
+FROM( 
+    SELECT COUNT(CLIENT_ID) AS COUNTING, CLIENT_ID
+    FROM project
+    GROUP BY CLIENT_ID
+) AS T 
+    );
